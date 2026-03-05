88f4ecf84210464070bfac4a024558602d45b6d0
Nicolas Gomez <nicolasagomezzzz@gmail.com>
2026-03-05 19:12:46 -0300

    arreglo barra base y cname

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
     site: 'https://nicolasgomezzzz.github.io',
-    base: 'curso-tatuajes',
+    base: '/curso-tatuajes',
 });
