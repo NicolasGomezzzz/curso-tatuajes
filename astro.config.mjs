1653b9b3ac0c9c08811eec0bda7645cebb651759
Nicolas Gomez <nicolasagomezzzz@gmail.com>
2026-03-05 16:19:02 -0300

    link de config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.tudominio.com', // <--- Poné acá tu dominio de DonWeb
+    site: 'https://niciatattoo.com', // <--- Poné acá tu dominio de DonWeb
     base: '/',
 });
