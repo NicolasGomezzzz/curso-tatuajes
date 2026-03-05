d7440c16e9a7286affb9a5b45084010cd8d00420
Nicolas Gomez <nicolasagomezzzz@gmail.com>
2026-03-05 17:37:25 -0300

    fixear error 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://niciatattoo.com', // Tu dominio real
-    base: 'curso-tatuajes',          // EL NOMBRE EXACTO DE TU REPOSITORIO
+    site: 'https://nicolasgomezzzz.github.io',
+    base: 'curso-tatuajes',
 });
