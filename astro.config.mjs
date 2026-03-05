101d566cd081ea0c30c5fb04759b473c2dd4cad9
Nicolas Gomez <nicolasagomezzzz@gmail.com>
2026-03-05 15:04:05 -0300

    configuracion de dominio

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://www.tudominio.com', // <--- Poné acá tu dominio de DonWeb
+    base: '/',
+});
